44ce9cab281a55bd8d4a40ae4a12c1ba72bdedba
Scorpio <416842949@qq.com>
2019-01-17 11:10:36 +0800

    feat(N/A): 支持AndroidX的非空检查
    影响范围：N/A
    影响平台：N/A
    版本号: N/A
    影响细节&测试建议：
    N/A

--- lintlib/src/main/java/com/xgimi/lintlib/detector/CheckNullDetector.kt
+++ lintlib/src/main/java/com/xgimi/lintlib/detector/CheckNullDetector.kt
@@ -16,29 +16,33 @@ import java.util.*
  * desc:
  */
 class CheckNullDetector : Detector(), SourceCodeScanner {
-    
+
     companion object {
         private const val charNonNull = "NonNull"
         private const val charNullable = "Nullable"
         private const val androidAnnotations = "com.android.annotations."
         private const val supportAnnotation = "android.support.annotation."
         private const val rxAnnotation = "io.reactivex.annotations."
-        
+        private const val androidX = "androidx.annotation."
+
         private const val androidNonNull = androidAnnotations + charNonNull
         private const val androidNullable = androidAnnotations + charNullable
-        
+
         private const val supportNonNull = supportAnnotation + charNonNull
         private const val supportNullable = supportAnnotation + charNullable
-        
+
         private const val rxNonNull = rxAnnotation + charNullable
         private const val rxNullable = rxAnnotation + charNullable
-        
+        private const val androidXNonNull = androidX + charNullable
+        private const val androidXNullable = androidX + charNullable
+
         private val annotationArray = arrayOf(supportNonNull, supportNullable,
                 androidNonNull, androidNullable,
-                rxNonNull, rxNullable)
-        
+                rxNonNull, rxNullable,
+                androidXNonNull, androidXNullable)
+
         private val passArray = arrayOf("int", "double", "float", "short", "boolean", "byte", "long", "char")
-        
+
         @JvmField
         val ISSUE = Issue.create(ConfigurationTitle.addTitle(ConfigurationTitle.addTitle("参数")),
                 ConfigurationTitle.addTitle("请标记 @$charNullable 或 @$charNonNull"),
@@ -46,21 +50,21 @@ class CheckNullDetector : Detector(), SourceCodeScanner {
                 Category.CORRECTNESS, 4, Severity.WARNING,
                 Implementation(CheckNullDetector::class.java, Scope.JAVA_FILE_SCOPE))
     }
-    
+
     override fun getApplicableUastTypes(): List<Class<out UElement>>? {
         val types = ArrayList<Class<out UElement>>(1)
         types.add(UParameter::class.java)
         return types
     }
-    
-    
+
+
     override fun createUastHandler(@NonNull context: JavaContext): UElementHandler? {
         return AnnotationChecker(context)
     }
-    
+
     private inner class AnnotationChecker internal constructor(
             @param:NonNull private val mContext: JavaContext) : UElementHandler() {
-        
+
         override fun visitParameter(@NonNull node: UParameter) {
             var isCheckPass = false
             for (i in annotationArray.indices) {
@@ -73,14 +77,14 @@ class CheckNullDetector : Detector(), SourceCodeScanner {
             for (i in passArray.indices) {
                 if (typeName.contains(passArray[i])) return
             }
-            
+
             if (!isCheckPass) mContext.report(ISSUE, node.uastAnchor, mContext.getLocation(node.uastAnchor!!),
                     ConfigurationTitle.addTitle("请标记 @$charNullable 或 @$charNonNull"),
                     initFixGroup(mContext, node)
             )
         }
     }
-    
+
     private fun createLintFix(mContext: JavaContext, node: UParameter, with: String): LintFix {
         val location = mContext.getLocation(node.typeElement!!)
         val name = getTypeName(node)
@@ -92,18 +96,18 @@ class CheckNullDetector : Detector(), SourceCodeScanner {
                 .with("@$with $name")
                 .build()
     }
-    
+
     private fun initFixGroup(mContext: JavaContext, node: UParameter): LintFix {
         return createFixGroup(
                 createLintFix(mContext, node, charNullable),
                 createLintFix(mContext, node, charNonNull)
         )
     }
-    
+
     private fun createFixGroup(vararg lintFix: LintFix): LintFix {
         return LintFix.create().group(*lintFix)
     }
-    
+
     private fun getTypeName(node: UParameter): String {
         return node.text!!.replace(" " + node.name!!, "")
     }
